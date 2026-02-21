babd7206fc8e4d4eeb73c59fe59008bb201ac765
Your Name <you@example.com>
2026-02-21 10:11:26 -0500

    feature: Buton & loader

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from './components/shared/button/button';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Button } from '../../shared/button/button';
+import { Loader } from '../../shared/loader/loader';
 
 @Component({
   selector: 'app-home',
-  imports: [Button],
+  standalone: true,
+  imports: [Button, Loader],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
