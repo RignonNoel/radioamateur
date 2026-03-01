9a8b10a7b6420267b8ee9315932f39982392aef9
Your Name <you@example.com>
2026-03-01 18:32:08 -0500

    feature:snackbar

--- src/app/components/shared/snackbar/snackbar.ts
+++ src/app/components/shared/snackbar/snackbar.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatSnackBarRef, MAT_SNACK_BAR_DATA, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-snackbar',
-  imports: [],
+  standalone: true,
+  imports: [MatSnackBarModule, MatIconModule, MatButtonModule],
   templateUrl: './snackbar.html',
-  styleUrl: './snackbar.scss',
+  styleUrls: ['./snackbar.scss'],
 })
-export class Snackbar {
-
+export class Snackbar implements OnInit {
+  constructor(
+    public sbRef: MatSnackBarRef<Snackbar>,
+    @Inject(MAT_SNACK_BAR_DATA) public data: any,
+  ) {}
+  ngOnInit() {}
 }
