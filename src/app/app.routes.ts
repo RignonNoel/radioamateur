81e066f19f5cf52f85f1f0b17de7ad2d0f457ccf
Your Name <you@example.com>
2026-03-01 16:37:44 -0500

    test Angular Schematics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
+import { Default } from './components/layouts/default/default';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Default, children: [{ path: '', component: Home }] },
+];
