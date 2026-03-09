1ed97838a324db20c0ceb371d87ca806dd79fd33
Your Name <you@example.com>
2026-03-09 18:29:12 -0400

    Feature: Page examens et icone bouton Connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
 import { Default } from './components/layouts/default/default';
+import { ExamGenerator } from './components/pages/exam-generator/exam-generator';
 
 export const routes: Routes = [
-  { path: '', component: Default, children: [{ path: '', component: Home }] },
+  {
+    path: '',
+    component: Default,
+    children: [
+      { path: '', component: Home },
+      { path: 'examens', component: ExamGenerator },
+    ],
+  },
 ];
---
+++ src/app/components/pages/exam-generator/exam-generator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-generator',
+  imports: [],
+  templateUrl: './exam-generator.html',
+  styleUrl: './exam-generator.scss',
+})
+export class ExamGenerator { }
