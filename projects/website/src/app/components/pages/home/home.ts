1ef2e6e6fce0af415086e789196e66df540a9447
Your Name <you@example.com>
2026-03-31 21:16:29 -0400

    Feature: Homepage hero - not finish

--- projects/website/src/app/components/pages/home/home.ts
+++ projects/website/src/app/components/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Button } from '../../shared/button/button';
 import { Loader } from '../../shared/loader/loader';
+import { Hero } from '../../shared/hero/hero';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Button, Loader],
+  imports: [Button, Loader, Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
